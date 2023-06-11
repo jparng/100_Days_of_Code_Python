bfbfeedecde1b2f18b126e0b9aa8fda369f25a3a
Jerry Parng <59073672+jparng@users.noreply.github.com>
2023-06-11 15:55:07 -0500

    Update Day_4.py

--- Day_4.py
+++ Day_4.py
@@ -34,12 +34,16 @@ player = [rock, paper, scissors]
 computer = [rock, paper, scissors]
 play = computer[random.randint(0,2)]
 
-print(player[user])
-print("Computer chose:")
-print(play)
-if player[user] == rock and play == rock or player[user] == paper and play == paper or player[user] == scissors and play == scissors:
-  print("It's a draw.")
-elif player[user] == rock and play == paper or player[user] == paper and play == scissors or player[user] == scissors and play == rock:
-  print("You lose.")
-elif player[user] == scissors and play == paper or player[user] == paper and play == rock or player[user] == rock and play == scissors:
-  print("You Win!")
+if user >= 3:
+  print("Invalid input. Game over.")
+
+else:
+  print(player[user])
+  print("Computer chose:")
+  print(play)
+  if player[user] == rock and play == rock or player[user] == paper and play == paper or player[user] == scissors and play == scissors:
+    print("It's a draw.")
+  elif player[user] == rock and play == paper or player[user] == paper and play == scissors or player[user] == scissors and play == rock:
+    print("You lose.")
+  elif player[user] == scissors and play == paper or player[user] == paper and play == rock or player[user] == rock and play == scissors:
+    print("You Win!")
